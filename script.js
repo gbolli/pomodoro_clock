02a5e2423406086846acb0e4197d83137a0482a7
Gord Bolli <gord@bolli.ca>
2017-07-26 21:53:59 -0700

    added button functionality to add, subtract minutes

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
 
     // Set variables
+    var timer = 25;
+    var session = 25;
+    var breakLength = 5;
 
+    $("#timer").text(timer);
 
     // Break length setting
+    $("button").click(function() {
+        var action = $(this).attr('id');
+        if (action === "sminus") {
+            session--;
+            $("#stotal").text(session);
+        }
+        else if (action === "splus") {
+            session++;
+            $("#stotal").text(session);
+        }
+        else if (action === "bminus") {
+            breakLength--;
+            $("#btotal").text(breakLength);
+        }
+        else {
+            breakLength++;
+            $("#btotal").text(breakLength);
+        }
+        
+        timer = session;
 
+        //  something different if timer is running ??
+
+    })
 
     // Session length setting
 
