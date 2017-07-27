59fee4eec766a55a299265b652b2b6f66474c75f
Gord Bolli <gord@bolli.ca>
2017-07-26 19:46:13 -0700

    repositioned timer, moved image to background, added js outline

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
-    
+    // Set variables
+
+
+    // Break length setting
+
+
+    // Session length setting
+
+
+    // Start timer
+
+
+    // When timer is finished, play alarm
+
+
+    // Reset
+
+
+    // Progress bar?
+
 
 });
